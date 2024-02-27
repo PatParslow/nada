35f850e0a641dcba17f807e03d8f0e2b66dac147
Pat Parslow <p.parslow@reading.ac.uk>
2024-02-27 06:49:55 +0000

    re-ordered the assignments to make it clearer

--- ref-check.cpp
+++ ref-check.cpp
@@ -8,31 +8,28 @@ class A {
     static int _id;
     int _my_id;
     int _parent_id;
-    std::vector<int> _copy_ids;
+    int _copy_id;
 public:
     int id() { return this->_my_id; }
     int parent_id() { return this->_parent_id; }
-    const std::vector<int>& copy_ids() { return this->_copy_ids; }
-    A() : _parent_id(0) { _my_id=_id++; std::cout << "A constructor "  << _my_id << std::endl; }
-    A(const A& other) : _my_id(_id++), _parent_id(other._my_id) { 
-        //empty the copy_ids vector
-        _copy_ids.clear();
-        std::cout << "A copy constructor "  << _my_id << " parent id " << _parent_id << std::endl; 
+    int copy_id() { return this->_copy_id; }
+    A() : _parent_id(0), _copy_id(0) { _my_id=_id++; std::cout << "A constructor "  << _my_id << std::endl; }
+    A(const A& other) : _my_id(_id++), _parent_id(other._my_id), _copy_id(other._copy_id + 1) { 
+        std::cout << "A copy constructor "  << _my_id << " parent id " << _parent_id << " copy id " << _copy_id << std::endl; 
     }
-        A(A&& other) noexcept : _my_id(other._my_id), _parent_id(other._parent_id) {
-        other._my_id = 0;
-        other._parent_id = 0;
+    A(A&& other) noexcept : _my_id(other._my_id), _parent_id(other._parent_id), _copy_id(other._copy_id) {
     }
     A& operator=(A&& other) noexcept {
         if (this != &other) {
             _my_id = other._my_id;
             _parent_id = other._parent_id;
-            other._my_id = 0;
-            other._parent_id = 0;
+            _copy_id = other._copy_id;
         }
         return *this;
     }
-    ~A() { std::cout << "\033[1;31m" << "A destructor "  << _my_id << " parent id " << _parent_id << "\033[0m"<< std::endl; }
+    ~A() { 
+        std::cout << "\033[1;31m" << "A destructor "  << _my_id << " parent id " << _parent_id << " copy id " << _copy_id << "\033[0m"<< std::endl; 
+    }
 };
 int A::_id = 1;
 
@@ -53,44 +50,49 @@ public:
     }
     std::vector<A>& getStore() { return _store; }
 };
-// we need to show how object go out of scope, so can't use references or pointers
+
+
+// let's create some objects in functions, to show how they are created and destroyed
 A* createA() {
     A* a = new A();
-    std::cout << "createA " << a->id() << " in createA"<< std::endl;
+    std::cout << "create A #" << a->id() << " in createA"<< std::endl;
     return a;
 }
 
 A& createARef() {
     A* a = new A();
-    std::cout << "createARef " << a->id() << " in createARef"<< std::endl;
+    std::cout << "create ARef #" << a->id() << " in createARef"<< std::endl;
     return *a;
 }
 
 std::unique_ptr<A> createAUnique() {
     std::unique_ptr<A> a = std::make_unique<A>();
-    std::cout << "createAUnique " << a->id() << " in createAUnique"<< std::endl;
+    std::cout << "create AUnique #" << a->id() << " in createAUnique"<< std::endl;
     return a;
 }   
 
 void make_them_all(B& b) {
-    std::cout << "create an object 'a' ";
-    A a;
+
     std::cout << "create an object 'a_ptr' ";
     A* a_ptr = createA();
-    std::cout << "create an object 'a_ref' "; 
-    A& a_ref = createARef();
-    std::cout << "create an object 'a_unique' ";
-    std::unique_ptr<A> a_unique = createAUnique();
-    //add them to store without creating new objects
-
-    std::cout << "add 'a' by value ";
-    b.add(a); 
     std::cout << "add 'a_ptr' by (pointer)value ";
     b.add(a_ptr); 
+
+    std::cout << "create an object 'a_ref' "; 
+    A& a_ref = createARef();
     std::cout << "add 'a_ref' by reference "; 
     b.add_byref(a_ref); 
+
+    std::cout << "create an object 'a_unique' ";
+    std::unique_ptr<A> a_unique = createAUnique();
     std::cout << "add 'a_unique' by unique_ptr using move to transfer ownership "; 
     b.add(std::move(a_unique)); 
+
+    std::cout << "create an object 'a' ";
+    A a;
+    std::cout << "add 'a' by value ";
+    b.add(a); 
+
     //show the store
     b.show();
 
